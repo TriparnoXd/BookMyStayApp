c802499036202f568a4c479ef1b22f26be198a54
vanshmoudgil22 <vanshmoudgil22@email.com>
2026-02-24 16:10:59 +0530

    UC3Palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,17 +1,18 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         String input="madam";
-        boolean isPalindrome=true;
-        for(int i=0;i<input.length()/2;i++){
-            if(input.charAt(i)!=input.charAt(input.length()-1-i)){
-                isPalindrome=false;
-                break;
+        String reversed = ""; // String objects are immutable
 
-            }
-
-
+        // Step 2: Reverse string using loop
+        // Hint: Iterate from the last character to the first
+        for (int i = input.length() - 1; i >= 0; i--) {
+            // String Concatenation (+) to build the reversed string
+            reversed = reversed + input.charAt(i);
         }
-        if (isPalindrome) {
+
+        // Step 3: Compare original and reversed using equals() method
+        // We use .equals() to compare actual content instead of memory references
+        if (input.equals(reversed)) {
             System.out.println("The string \"" + input + "\" is a palindrome.");
         } else {
             System.out.println("The string \"" + input + "\" is NOT a palindrome.");
